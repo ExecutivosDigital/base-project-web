373699fea031e3bd3d82c86cc4706867c62c2e78
Gabriel <85517869+GabrielFernandes123@users.noreply.github.com>
2025-03-30 16:08:28 -0400

    style(style): change import sort

--- src/context/SampleContext.tsx
+++ src/context/SampleContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { createContext, useContext } from "react";
 import { useCookies } from "next-client-cookies";
+import { createContext, useContext } from "react";
 
 interface SampleContextProps {
   token: string | undefined;
